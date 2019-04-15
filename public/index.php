4824099170de1787e0fa854f1d36fdf193408a8d
ahmad luky ramdani <ahmadluky@apps.ipb.ac.id>
2019-04-15 15:51:53 +0700

    get data harga, judul, deskripsi dan link gambar dari tokped

--- public/index.php
+++ public/index.php
@@ -10,13 +10,27 @@ require BASE_DIR . '/vendor/autoload.php';
 use Goutte\Client;
 
 $url = "https://www.tokopedia.com/csiindo/samsung-galaxy-note-9-128gb-handphone-bekas-grade-c?src=topads";
-$css_selector = "h1.rvm-product-title";
+$css_selector_judul = "h1.rvm-product-title";
+$css_selector_deskripsi = ".product-summary__content.active";
+$css_selector_img = ".content-img-relative img";
+$css_selector_harga = ".rvm-price.mr-15 span";
+
 $thing_to_scrape = "_text";
+$thing_to_scrape_img = "src";
 
 $client = new Client();
 $crawler = $client->request('GET', $url);
-$output = $crawler->filter($css_selector)->extract($thing_to_scrape);
+$judul = $crawler->filter($css_selector_judul)->extract($thing_to_scrape);
+$deskripsi = $crawler->filter($css_selector_deskripsi)->extract($thing_to_scrape);
+$img = $crawler->filter($css_selector_img)->extract($thing_to_scrape_img);
+$harga = $crawler->filter($css_selector_harga)->extract($thing_to_scrape);
 
-var_dump($output);
+$tokped = array(
+                    'judul'=> $judul[0],
+                    'deskripsi' => $deskripsi[0],
+                    'image' => $img,
+                    'harga' => $harga[1]
+);
+echo json_encode($tokped);
 
 ?>
